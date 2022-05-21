3720578265fc1e03a8a19e7913ad515a9eb8dc47
T.S <tauras.strukas@protonmail.com>
2022-05-21 19:28:43 +0300

    init Button + cleanup

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+
+export const Default = () => (
+  <Button
+    handleClick={() => {
+      //
+    }}
+  >
+    My Button
+  </Button>
+);
+
+export default {
+  title: 'Button',
+  component: Button,
+};
---
+++ src/components/Button.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+background-color: #000;
+border: none;
+color: #fff;
+padding: 1rem;
+`
