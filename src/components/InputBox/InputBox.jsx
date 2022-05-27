94fdb02a20ecbb43ea892cfe0da8f453153ae5be
T.S <tauras.strukas@protonmail.com>
2022-05-27 20:45:58 +0300

    pass InputBox props

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -2,8 +2,32 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import * as S from './InputBox.styles';
 
-const InputBox = () => {
-  return <S.InputBox>InputBox</S.InputBox>;
+const InputBox = ({ name, placeholder, label, initValue, handleChange }) => {
+  const [value, setValue] = useState(initValue || '');
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    handleChange(e.target.value);
+  };
+  return (
+    <>
+      <S.InputBox
+        name={name}
+        placeholder={placeholder}
+        label={label}
+        onChange={onChange}
+        value={value}
+      />
+    </>
+  );
+};
+
+InputBox.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  initValue: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
 };
 
 export default InputBox;
