fb718b804887d8502ffe34c3f25ddd2254e0cd2d
T.S <tauras.strukas@protonmail.com>
2022-05-28 15:23:13 +0300

    add inputbox story - not working atm

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -2,32 +2,36 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import * as S from './InputBox.styles';
 
-const InputBox = ({ name, placeholder, label, initValue, handleChange }) => {
-  const [value, setValue] = useState(initValue || '');
+const InputBox = ({ name, placeholder, label, handleChange }) => {
+  const [value, setValue] = useState('');
 
   const onChange = (e) => {
     setValue(e.target.value);
     handleChange(e.target.value);
   };
+
   return (
     <>
+      <S.Label htmlFor={name}>{label}</S.Label>
       <S.InputBox
         name={name}
         placeholder={placeholder}
-        label={label}
-        onChange={onChange}
         value={value}
+        onChange={onChange}
       />
     </>
   );
 };
 
 InputBox.propTypes = {
-  handleChange: PropTypes.func.isRequired,
-  initValue: PropTypes.string,
-  label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  label: PropTypes.string,
   placeholder: PropTypes.string,
+};
+
+InputBox.defaultProps = {
+  label: '',
+  placeholder: '',
 };
 
 export default InputBox;
---
+++ src/components/InputBox/InputBox.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import InputBox from './InputBox';
+
+export default {
+  title: 'InputBox',
+  component: InputBox,
+};
+
+export const Default = () => (
+  <InputBox
+    label="Pulling"
+    name="Exercise"
+    placeholder="Pull-Up"
+    onChange={() => {
+      //   console.log('Test');
+    }}
+  />
+);
