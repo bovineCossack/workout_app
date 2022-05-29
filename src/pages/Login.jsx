1eaeef27c4fcf8f6723d88e3ce8e1d3faa0a1ba7
T.S <tauras.strukas@protonmail.com>
2022-05-29 12:23:17 +0300

    fill in Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { InputBox } from '../components/InputBox';
+import Content from '../components/Content/Content';
+import { Header } from '../components/Header';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/v1/auth/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        }
+      );
+      const data = await res.json();
+
+      alert(data.msg || data.err || 'Unknown error');
+      if (data.msg === 'Incorrect data entered. Please revise.' || data.error) {
+        return;
+      } else {
+        navigate('/');
+      }
+    } catch (error) {
+      alert(error.message || 'Unknown error');
+    }
+  };
+  return (
+    <>
+      <Header>
+        <Link to="/" className="home">
+          Home
+        </Link>
+      </Header>
+      <Content>
+        <form onSubmit={onLogin}>
+          <InputBox
+            name="email"
+            placeholder="your@email.com"
+            label="Email"
+            type="email"
+            handleChange={(value) => setUserData({ ...userData, email: value })}
+          />
+          <InputBox
+            name="password"
+            placeholder="Password"
+            label="Password"
+            type="password"
+            handleChange={(value) =>
+              setUserData({ ...userData, password: value })
+            }
+          />
+          <Button>Login</Button>
+        </form>
+      </Content>
+    </>
+  );
 };
 
 export default Login;
