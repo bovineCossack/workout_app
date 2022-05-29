e43582be9955d3d8a5e12547944f8abfe6a64c7b
T.S <tauras.strukas@protonmail.com>
2022-05-29 12:41:57 +0300

    add dummy ExerciseList

--- src/components/ExerciseList/ExerciseList.jsx
+++ src/components/ExerciseList/ExerciseList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import * as S from './ExerciseList.styles';
+import Exercise from '../Exercise/Exercise';
 
-const ExerciseList = () => {
-  return <div>ExerciseList</div>;
+const ExerciseList = ({ exercises, handleClick }) => {
+  return (
+    <S.ExerciseList>
+      {exercises &&
+        exercises.map((exercise) => (
+          <Exercise>
+            <S.ExerciseName>{`${exercise.name}`}</S.ExerciseName>
+            <S.ExerciseCategory>{`Primary muscle group: ${exercise.category1}`}</S.ExerciseCategory>
+            <S.ExerciseCategory>{`Secondary muscle group: ${exercise.category2}`}</S.ExerciseCategory>
+          </Exercise>
+        ))}
+    </S.ExerciseList>
+  );
 };
-
-export default ExerciseList;
