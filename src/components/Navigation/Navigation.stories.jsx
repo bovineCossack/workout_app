1d3e4abaad31f9cefbf5e2b14dd921d69467301b
T.S <tauras.strukas@protonmail.com>
2022-05-31 11:28:01 +0300

    remove redundant component

--- src/components/Navigation/Navigation.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Navigation from './Navigation';
-
-export default {
-  title: 'Navigation',
-  component: Navigation,
-};
-
-export const Primary = () => <Navigation to="/register">Register</Navigation>;
