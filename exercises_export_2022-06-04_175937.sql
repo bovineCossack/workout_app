f0ee2161686ae98a457f93047c4652be3c40e0bf
T.S <tauras.strukas@protonmail.com>
2022-06-04 22:23:17 +0300

    remove sql, make dropdown

--- exercises_export_2022-06-04_175937.sql
+++
@@ -1,1 +0,0 @@
-insert into `exercises` (`category1`, `category2`, `created_at`, `id`, `name`) values ('Chest', 'Triceps', '2022-06-03 11:16:59', 1, 'Push-Up');
---
+++ src/components/UI/DropDown/DropDown.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as S from './DropDown.styles';
+
+const DropDown = () => {
+  return <S.Dropdown>DropDown</S.Dropdown>;
+};
+
+export default DropDown;
