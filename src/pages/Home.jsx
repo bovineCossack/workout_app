f3bd1cbd37602514c91ba144d3483aa4f545f6b6
T.S <tauras.strukas@protonmail.com>
2022-06-04 21:09:02 +0300

    cleaned up Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,7 @@
 import ExerciseList from '../components/ExerciseList/ExerciseList';
-import { Button } from '../components/UI/Button/Button.styles';
 import Content from '../components/UI/Content/Content';
-import AuthContext from '../store/authContext';
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let navigate = useNavigate();
-  const authCtx = useContext(AuthContext);
-
-  function logout() {
-    authCtx.logout();
-    setIsLoggedIn(false);
-    localStorage.removeItem('token');
-    navigate('/', { redirect: true });
-    console.log('logout === logout');
-  }
-
   return (
     <Content>
       <h2>Your Exercises</h2>
