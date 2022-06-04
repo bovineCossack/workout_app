0345cb94f0a07a63a2b42fed9f302d1ab818a02e
bovineCossack <85243268+bovineCossack@users.noreply.github.com>
2022-06-04 18:00:27 +0300

    add sql

---
+++ exercises_export_2022-06-04_175937.sql
@@ -0,0 +1,1 @@
+insert into `exercises` (`category1`, `category2`, `created_at`, `id`, `name`) values ('Chest', 'Triceps', '2022-06-03 11:16:59', 1, 'Push-Up');
---
+++ users_export_2022-06-04_175952.sql
@@ -0,0 +1,3 @@
+insert into `users` (`created_at`, `email`, `id`, `password`) values ('2022-06-03 11:16:34', 'test@test.com', 1, '$2a$10$UUXwXrdqeeqos65QUfPWNOXLFGykMl2R/P2JKqp9BuMbChabUqdkC');
+insert into `users` (`created_at`, `email`, `id`, `password`) values ('2022-06-04 16:58:09', 'john@smith.com', 2, '$2a$10$Epou7q4kpVEi6NU1fiiImeSk7BYcAcDun5584RM4Uoj8p1tCo0SrS');
+insert into `users` (`created_at`, `email`, `id`, `password`) values ('2022-06-04 17:20:21', 'tauras@strukas.lt', 3, '$2a$10$exNalUkQqyUgF40iatWnr.YRPwF8QiqARBXNRMrbuwdYFg1/aELOy');
